89c23e595ead5c76b3e23f749da193950733b8d2
vinbh <vinayak.farcry@gmail.com>
2022-03-06 17:26:20 +0530

    Update pickle.py

--- pickle.py
+++ pickle.py
@@ -1,6 +1,6 @@
 import shelve,pickle
 
-with  open("/Users/vinayak.bhatt/Downloads/Airplane_Crashes_and_Fatalities_Since_1908.csv") as csv_file:
+with  open("/Users/fb/Downloads/Airplane_Crashes_and_Fatalities_Since_1908.csv") as csv_file:
     csv_dict = pickle.load(csv_file)
 
 for var in csv_dict:
